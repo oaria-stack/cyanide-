091b5f935f52a77c184634909530fefc0a97107c
oaria-stack <kovacsanamaria@gmail.com>
2026-05-23 16:10:17 +0300

    unzipgit

---
+++ Cyanide/main.m
@@ -0,0 +1,18 @@
+//
+//  main.m
+//  Cyanide
+//
+//  Created by seo on 3/24/26.
+//
+
+#import <UIKit/UIKit.h>
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[]) {
+    NSString * appDelegateClassName;
+    @autoreleasepool {
+        // Setup code that might create autoreleased objects goes here.
+        appDelegateClassName = NSStringFromClass([AppDelegate class]);
+    }
+    return UIApplicationMain(argc, argv, nil, appDelegateClassName);
+}
