674eb19c8cf92bb4879f09ae44ec116dcd97ef19
oaria-stack <kovacsanamaria@gmail.com>
2026-05-23 15:43:18 +0300

    unzipgit

---
+++ cyanide-ios-improved/Cyanide/installer/InstallProgressViewController.h
@@ -0,0 +1,16 @@
+//
+//  InstallProgressViewController.h
+//  Cyanide
+//
+//  Sileo-style install progress sheet: live log + spinner during apply, then
+//  "Done" once settings_run_actions completes.
+//
+
+#import <UIKit/UIKit.h>
+
+NS_ASSUME_NONNULL_BEGIN
+
+@interface InstallProgressViewController : UIViewController
+@end
+
+NS_ASSUME_NONNULL_END
