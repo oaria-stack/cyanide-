7f92bd93ea7055552155f6c493375035b99b4c03
oaria-stack <kovacsanamaria@gmail.com>
2026-05-23 16:09:56 +0300

    unzipgit

---
+++ Cyanide/installer/QueueReviewViewController.h
@@ -0,0 +1,16 @@
+//
+//  QueueReviewViewController.h
+//  Cyanide
+//
+//  Sileo "DownloadsTableViewController" analog: shows the queued
+//  install/uninstall operations and offers CONFIRM / CLEAR actions.
+//
+
+#import <UIKit/UIKit.h>
+
+NS_ASSUME_NONNULL_BEGIN
+
+@interface QueueReviewViewController : UIViewController
+@end
+
+NS_ASSUME_NONNULL_END
